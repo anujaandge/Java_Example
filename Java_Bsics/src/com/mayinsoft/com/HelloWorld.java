58f54653a77dba779f2af57a6bb6e667a1edd7dc
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-07-23 19:18:26 +0530

    new changes

--- Java_Bsics/src/com/mayinsoft/com/HelloWorld.java
+++ Java_Bsics/src/com/mayinsoft/com/HelloWorld.java
@@ -2,6 +2,7 @@ package com.mayinsoft.com;
 
 public class HelloWorld {
 		public static void main(String[] args) {
-			System.out.println("Hello World");
+			int i =0;
+			System.out.println("Hello World" + i );
 		}
 }
