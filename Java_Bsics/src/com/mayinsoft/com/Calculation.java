fd02fe0be4ba5a380c9e03547e47d56bd04dbca7
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-07-24 12:42:38 +0530

    new file added calculation

---
+++ Java_Bsics/src/com/mayinsoft/com/Calculation.java
@@ -0,0 +1,23 @@
+package com.mayinsoft.com;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=6;
+		int sum=a+b;
+		int sub=a-b;
+		int mul=a*b;
+		int div=a/b;
+		int mod=a%b;
+		System.out.println("Addition: "+sum);
+		System.out.println("Subtraction: "+sub);
+		System.out.println("Multiplication: "+mul);
+		System.out.println("Division: "+div);
+		System.out.println("Modulus: "+mod);
+
+	}
+
+}
+
