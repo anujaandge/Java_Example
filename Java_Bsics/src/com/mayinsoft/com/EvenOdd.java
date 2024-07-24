7e7017095c7e924669f4b5759c7d555716954ff2
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-07-24 16:53:19 +0530

    used ternary operator

--- Java_Bsics/src/com/mayinsoft/com/EvenOdd.java
+++ Java_Bsics/src/com/mayinsoft/com/EvenOdd.java
@@ -5,11 +5,12 @@ public class EvenOdd {
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Enter nuber to check even or odd: ");
 		int num=sc.nextInt();
-		if(num%2==0) {
-			System.out.println(num+" is Even number.");
-		}
-		else {
-			System.out.println(num+" is odd number.");
-		}
+//		if(num%2==0) {
+//			System.out.println(num+" is Even number.");
+//		}
+//		else {
+//			System.out.println(num+" is odd number.");
+//		}
+		System.out.println((num%2==0) ? "this is even number" : "this is odd number");
 	}
 }
