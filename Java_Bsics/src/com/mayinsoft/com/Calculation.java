4cab9b39a7aae8aed157182fcd0065845b3aa141
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-07-29 15:53:04 +0530

    some changes done

--- Java_Bsics/src/com/mayinsoft/com/Calculation.java
+++ Java_Bsics/src/com/mayinsoft/com/Calculation.java
@@ -1,5 +1,7 @@
 package com.mayinsoft.com;
 
+import java.util.StringTokenizer;
+
 public class Calculation {
 
 	public static void main(String[] args) {
@@ -11,15 +13,42 @@ public class Calculation {
 		int mul=a*b;
 		int div=a/b;
 		int mod=a%b;
-		System.out.println("Addition: "+sum);
-		System.out.println("Subtraction: "+sub);
-		System.out.println("Multiplication: "+mul);
-		System.out.println("Division: "+div);
-		System.out.println("Modulus: "+mod);
+		//System.out.println("Addition: "+sum);
+		//System.out.println("Subtraction: "+sub);
+		//System.out.println("Multiplication: "+mul);
+		//System.out.println("Division: "+div);
+		//System.out.println("Modulus: "+mod);
 		//string concationation
-		String s="Anuja";
-		String s1="Andge";
-		System.out.println("Concatination: "+s+" "+s1);
+		//String s="Anuja";
+//		String s1="Andge";
+//		System.out.println("Concatination: "+s+" "+s1);
+		short i=129;
+		short j=129;
+		//System.out.println(i==j);
+		
+		String s1 = "ABC" + 1;
+		String s2 = 130 + "ABC";
+		String s3 = new String("ABC");
+		String s4 = new String("ABC").intern();
+		/*
+		 * System.out.println(s1); System.out.println(s2);
+		 */
+		StringTokenizer s = new StringTokenizer("apple,ball,cat,dog",",");
+		while (s.hasMoreElements()) {
+			System.out.println(s.nextToken());
+			
+		}
+//		System.out.println(s);
+		
+		/*
+		 * System.out.println(s1==s4);
+		 * 
+		 * 
+		 * System.out.println(s1.equals(s3)); System.out.println(s1==s3);
+		 * System.out.println(s1==s2);
+		 */
+		
+		
 
 	}
 
