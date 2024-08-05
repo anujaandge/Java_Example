3aa712286b70e7ee4338a1408a0fe41142017e5d
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-08-05 11:00:30 +0530

    Exception Overriding

--- Java_Practice/src/main/java/com/mayinsoft/com/ExceptionOverinding.java
+++ Java_Practice/src/main/java/com/mayinsoft/com/ExceptionOverinding.java
@@ -1,8 +1,12 @@
 package com.mayinsoft.com;
-//Rules for Exception handling in ovrriding
+//Rules for Exception handling in overriding
 //1.when super class does not declares exception and subclass declares
-//i)when super classs does not declares exception and subclass declares checked exception=> compile time error occours
-//ii)when super classs does not declares exception and subclass declares unchecked exceptions=>code runs smmothly if exception occours then it will show that
+//i)when super class does not declares exception and subclass declares checked exception=> compile time error occurs
+//ii)when super class does not declares exception and subclass declares unchecked exceptions=>code runs smoothly if exception occurs then it will show that
+//2.When super class throws exception 
+//i)SubClass declares exceptions other than the child exception of the SuperClass declared Exception.=>compile time error
+//ii)Subclass declares exceptions which is the child of the superclass declared exception=>code runs smoothly if exception occurs then it will show that
+//iii)If SuperClass declares an exception and SubClass declares without exception.=>code runs smoothly if exception occurs then it will show that
 class Animal{
 	void eat() {
 		System.out.println("Animal is eating..");
@@ -16,9 +20,18 @@ class Dog extends Animal{
 		System.out.println(s.charAt(0));
 	}
 }
+class SuperClass {
+	void m1() throws RuntimeException{
+		System.out.println("method one..");
+	}
+}
+class Subclass extends SuperClass{
+	@Override
+	void m1() throws ArithmeticException{
+		
+	}
+}
 public class ExceptionOverinding {
-
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Animal a=new Animal();
